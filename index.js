76db596c80d7d7b009bf463e83dd958e2e099e26
adeeramashkoor30 <adeeramashkoor@gmail.com>
2025-08-04 17:59:37 +0500

    add 3 & 4 chapter

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ var message = "JavaScript is amazing!";
 alert (message);
 var age = "19";
 alert (age);
+alert("hellow");
+var $dollar = "8$"
+alert("$dollar");
+var user123 = "sara"
+alert("user123");
+var _underscore = "hyphrn allowed in the first"
+alert("_underscore");
+var 123user;      // starts with number
+var alert;        // reserved word
+var my name;      // space not allowed
+
+
 
